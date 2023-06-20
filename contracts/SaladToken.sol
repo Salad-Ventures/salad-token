d8eabe7ddfaf372a554a02e8cda987509ea093e5
Steven Khong <khongwh@gmail.com>
2023-06-20 10:44:27 +0800

    make token supply variable constant

--- contracts/SaladToken.sol
+++ contracts/SaladToken.sol
@@ -10,7 +10,7 @@ contract SaladToken is Ownable, ERC20, ERC20Burnable, ERC20Pausable {
   string private constant NAME = "Salad";
   string private constant SYMBOL = "SALD";
 
-  uint public TOTAL_SUPPLY = 1_680_000_000e18; // 1.68b SALD
+  uint public constant TOTAL_SUPPLY = 1_680_000_000e18; // 1.68b SALD
 
   constructor() ERC20(NAME, SYMBOL) {
     _mint(owner(), TOTAL_SUPPLY);
